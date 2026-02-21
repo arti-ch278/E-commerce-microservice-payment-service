3df3d0ffe6abb721190eed5af6e2e55be499d78d
Arti <arti.chourey21@gmail.com>
2026-02-21 21:38:07 +0530

    feat: add payment service entity

---
+++ src/main/java/com/artichourey/ecommerce/paymentservice/entity/Payment.java
@@ -0,0 +1,74 @@
+package com.artichourey.ecommerce.paymentservice.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import com.artichourey.ecommerce.paymentservice.enums.PaymentMethod;
+import com.artichourey.ecommerce.paymentservice.enums.PaymentStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="payments", indexes= {
+		@Index(columnList="orderId"),
+		@Index(columnList="transactionId")
+})
+
+
+
+public class Payment {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(nullable=false)
+	private String orderId;
+	@Column(nullable=false,precision=12, scale=2)
+	private BigDecimal amount;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable=false)
+	private PaymentStatus paymentStatus;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable=false)
+	private PaymentMethod paymentMethod;
+	@Column(unique=true,nullable=true)
+	private String transactionId;
+	private String gatewayPaymentId;
+
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+	@PrePersist
+	void createdOn() {
+		this.createdAt=LocalDateTime.now();
+		this.updatedAt=LocalDateTime.now();
+		
+	}
+	@PreUpdate
+	void updatedOn() {
+		this.updatedAt=LocalDateTime.now();
+	}
+
+}
