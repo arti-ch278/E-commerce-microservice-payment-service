09a3656022a6e9ee338dac8154cca18753b945fb
Arti <arti.chourey21@gmail.com>
2026-02-21 21:36:59 +0530

    feat: add payment service repository

---
+++ src/main/java/com/artichourey/ecommerce/paymentservice/repository/PaymentRepository.java
@@ -0,0 +1,14 @@
+package com.artichourey.ecommerce.paymentservice.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.artichourey.ecommerce.paymentservice.entity.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+	
+	Optional<Payment>findByOrderId(String orderId);
+	Optional<Payment>findByTransactionId(String transactionId);
+
+}
