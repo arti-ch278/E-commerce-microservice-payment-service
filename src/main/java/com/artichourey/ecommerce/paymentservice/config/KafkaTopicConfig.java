8d685642c54379f8fbaff28cf0be33594e473fa1
Arti <arti.chourey21@gmail.com>
2026-03-29 16:17:58 +0530

    feat(config): add KafkaTopicConfig for payment-related event topics

---
+++ src/main/java/com/artichourey/ecommerce/paymentservice/config/KafkaTopicConfig.java
@@ -0,0 +1,27 @@
+package com.artichourey.ecommerce.paymentservice.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+	
+	 @Bean
+	    public NewTopic paymentSuccessTopic() {
+	        return TopicBuilder.name("payment-success-topic")
+	                .partitions(3)
+	                .replicas(1)
+	                .build();
+	    }
+
+	    @Bean
+	    public NewTopic paymentFailedTopic() {
+	        return TopicBuilder.name("payment-failed-topic")
+	                .partitions(3)
+	                .replicas(1)
+	                .build();
+	    }
+
+}
