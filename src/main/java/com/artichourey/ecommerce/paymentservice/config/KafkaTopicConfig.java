32549077aa983a8dd4aaae506f1ae96aca39f06d
Arti <arti.chourey21@gmail.com>
2026-04-07 22:05:30 +0530

    feat: enhance Kafka topic configuration for payment events

--- src/main/java/com/artichourey/ecommerce/paymentservice/config/KafkaTopicConfig.java
+++ src/main/java/com/artichourey/ecommerce/paymentservice/config/KafkaTopicConfig.java
@@ -23,5 +23,12 @@ public class KafkaTopicConfig {
 	                .replicas(1)
 	                .build();
 	    }
+	    @Bean
+	    public NewTopic paymentRequestTopic() {
+	        return TopicBuilder.name("payment-request-topic")
+	                .partitions(3)
+	                .replicas(1)
+	                .build();
+	    }
 
 }
