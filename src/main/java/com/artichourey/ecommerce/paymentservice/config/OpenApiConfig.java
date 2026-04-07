b81d5c2699177a8758099f9d70ac3c270933bf82
Arti <arti.chourey21@gmail.com>
2026-04-07 22:06:21 +0530

    chore: refine OpenAPI configuration for better API documentation

--- src/main/java/com/artichourey/ecommerce/paymentservice/config/OpenApiConfig.java
+++ src/main/java/com/artichourey/ecommerce/paymentservice/config/OpenApiConfig.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
