a5f11573282c827425046265d581cf2cb4b58ae8
arti-ch278 <arti.chourey21@gmail.com>
2026-04-08 19:27:35 +0530

    Update KafkaProducerConfig , set headers true

--- src/main/java/com/artichourey/ecommerce/paymentservice/config/KafkaProducerConfig.java
+++ src/main/java/com/artichourey/ecommerce/paymentservice/config/KafkaProducerConfig.java
@@ -21,7 +21,7 @@ public class KafkaProducerConfig {
 	    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 	    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
-	    configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+	    configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, true);
 
 	    configProps.put(ProducerConfig.ACKS_CONFIG, "all");
 	    configProps.put(ProducerConfig.RETRIES_CONFIG, 5);
