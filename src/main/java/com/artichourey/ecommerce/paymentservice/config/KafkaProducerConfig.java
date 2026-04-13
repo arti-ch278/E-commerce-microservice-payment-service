047bc38c10b8817e41ea78bfa691d30bdcb8ed6c
Arti <arti.chourey21@gmail.com>
2026-04-13 21:24:35 +0530

    Enhance Kafka producer settings (serialization and retries)

--- src/main/java/com/artichourey/ecommerce/paymentservice/config/KafkaProducerConfig.java
+++ src/main/java/com/artichourey/ecommerce/paymentservice/config/KafkaProducerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -14,14 +15,18 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 public class KafkaProducerConfig {
 	
+	@Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+	
 	@Bean
 	public ProducerFactory<String, Object> producerFactory() {
 	    Map<String, Object> configProps = new HashMap<>();
-	    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+	    configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 	    configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 	    configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
-	    configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, false);
+	    configProps.put(JsonSerializer.ADD_TYPE_INFO_HEADERS, true);
 
 	    configProps.put(ProducerConfig.ACKS_CONFIG, "all");
 	    configProps.put(ProducerConfig.RETRIES_CONFIG, 5);
